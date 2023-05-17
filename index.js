9b89904bf4175cb91f8b5872fd326bcbd45b1ae6
Alvaro3c <alvaro.cuallado.g@gmail.com>
2023-05-17 22:01:14 +0200

    finished

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-let form = document.getElementById('quiz-form');
-const answers = Array.from(document.querySelectorAll('.answer'));
+const form = document.getElementById('quiz-form');
+const answersArray = Array.from(document.querySelectorAll('.answer'));
+const questionContainer = document.querySelectorAll('.question-container');
+const answers = document.querySelectorAll('.answer')
+const punctuation = document.querySelector('#punctuation')
+
+//Cosas a conseguir
+
+//resultado de cuantas preguntas han sido acertadas al hcer click en el boton
+//  Necesito el numero de preguntas, y el numero de preguntas acertadas
+//    El numero de preguntas acertadas es el numero de inputs checkeados que tienen valor true
+
+//si la pregunta es correcta que aparezca en verde cuando haga click en el boton
+
+//si la pregunta es incorrecta que aparezca en rojo cuando haga click en el boton
+
+//numero de preguntas:
+console.log(questionContainer.length)
+
+//answers
+console.log(answers[1].checked)
+
+
 
 form.addEventListener('submit', e => {
-    e.preventDefault()
-    const checkedAnswers = answers.filter(answer => answer.checked)
+    e.preventDefault();
+    //preguntas acertadas
+    let correctAnswers = []
+    for (let i = 0; i < answers.length; i++) {
 
-    checkedAnswers.forEach(answer => {
-        const isCorrect = answer.value === 'true'
-        const questionItem = answers.closest('.question-container')
-        if (isCorrect) {
-
-        } else {
-
+        if (answers[i].value === 'true' && answers[i].checked === true) {
+            correctAnswers.push('correct')
         }
-    })
-}) 
+    }
+    //Numero de Respuestas acertadas
+    console.log(correctAnswers.length)
+    let nOfCorrectAnswers = correctAnswers.length
+    let nOfTotalQuestions = questionContainer.length
+
+    let p = document.createElement('p');
+    p.innerHTML = `Tu puntuación es de ${nOfCorrectAnswers} preguntas acertadas de ${nOfTotalQuestions}`
+    punctuation.appendChild(p)
+
+})
